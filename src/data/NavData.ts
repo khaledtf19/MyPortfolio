35665b3503bd4b00e75fc10f6ecdfbcb77b1bcc1
Khaledtf Labeb <khaledtf19@gmail.com>
2023-03-13 18:40:23 +0200

    update home and skills

--- src/data/NavData.ts
+++ src/data/NavData.ts
@@ -6,7 +6,7 @@ interface NavDataType {
 
 const NavData: NavDataType[] = [
   { name: "Home", path: "#home", iconName: "name" },
-  { name: "About", path: "#about", iconName: "name" },
+  { name: "Skills", path: "#skills", iconName: "name" },
   { name: "Projects", path: "#projects", iconName: "name" },
   { name: "Contact", path: "#contact", iconName: "name" },
 ];
---
+++ src/data/SkillsData.ts
@@ -0,0 +1,28 @@
+export interface SkillType {
+  name: string;
+  iconName: string;
+}
+
+export const FrontSkills: SkillType[] = [
+  { name: "HTML", iconName: "HTML-icon" },
+  { name: "CSS", iconName: "CSS-icon" },
+  { name: "JavaScript", iconName: "Javascript-icon" },
+  { name: "TypeScript", iconName: "Typescript-icon" },
+  { name: "React", iconName: "React-icon" },
+  { name: "Next JS", iconName: "NextJS-icon" },
+  { name: "SASS", iconName: "SASS-icon" },
+  { name: "Tailwind", iconName: "Tailwind-icon" },
+  { name: "Botstrap", iconName: "Botstrap-icon" },
+];
+
+export const BackSkills: SkillType[] = [
+  { name: "JavaScript", iconName: "Javascript-icon" },
+  { name: "TypeScript", iconName: "Typescript-icon" },
+  { name: "Node JS", iconName: "NodeJS-icon" },
+  { name: "NextJS", iconName: "NextJS-icon" },
+  { name: "Nest JS", iconName: "NestJS-icon" },
+  { name: "Prisma", iconName: "Prisma-icon" },
+  { name: "tRPC", iconName: "tRPC-icon" },
+  { name: "MongoDB", iconName: "MongoDB-icon" },
+  { name: "PostgreSQL", iconName: "Postgresql-icon" },
+];
