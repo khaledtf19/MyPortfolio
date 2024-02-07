7bc5853976e6fce7c6decbe323d39397ab523ac4
Khaledtf Labeb <khaledtf19@gmail.com>
2024-02-07 20:18:46 +0200

    update astro icon to 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import icon from "astro-icon";
 // https://astro.build/config
 import react from "@astrojs/react";
 
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), icon()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
